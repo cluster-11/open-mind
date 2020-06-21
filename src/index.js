9ab6966ef83323acc1354c1880e0d90dd2d96f62
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-06-21 20:44:39 +0600

    Successfully completed part-1 - mousePressed is now working properly

--- src/index.js
+++ src/index.js
@@ -1,36 +1,3 @@
-// import p5 from "p5";
-// import ml5 from "ml5";
-
-// let video;
-// let features;
-// function setup() {
-//   createCanvas(320, 240);
-
-//   video = createCapture(VIDEO);
-
-//   video.size(320, 240);
-
-//   video.hide();
-
-//   features = ml5.featureExtractor("MobileNet", modelReady);
-// }
-
-// function mousePressed() {
-//   const logits = features.infer(video);
-//   console.log(logits.dataSync());
-//   logits.print();
-// }
-
-// function modelReady() {
-//   console.log("Model is ready");
-// }
-
-// function draw() {
-//   image(video, 0, 0);
-// }
-
-// console.log(p5);
-
 import p5 from "p5";
 import ml5 from "ml5";
 
@@ -41,8 +8,6 @@ const containerElement = document.getElementById("p5-container");
 
 const sketch = (p) => {
   p.setup = function () {
-    // p.createCanvas(800, 400);
-
     p.createCanvas(600, 400);
 
     video = p.createCapture(p5.VIDEO);
@@ -58,7 +23,7 @@ const sketch = (p) => {
     p.image(video, 0, 0);
   };
 
-  p5.mousePressed = function () {
+  p.mousePressed = function () {
     const logits = features.infer(video);
     console.log(logits.dataSync());
     logits.print();
