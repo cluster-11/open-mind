a8e0bec5724dbd3e40a42ec78d69af67927c1aa5
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-06-21 20:27:10 +0600

    Initial Commit from the ml5 web project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,72 @@
-console.log("Hello World");
+// import p5 from "p5";
+// import ml5 from "ml5";
+
+// let video;
+// let features;
+// function setup() {
+//   createCanvas(320, 240);
+
+//   video = createCapture(VIDEO);
+
+//   video.size(320, 240);
+
+//   video.hide();
+
+//   features = ml5.featureExtractor("MobileNet", modelReady);
+// }
+
+// function mousePressed() {
+//   const logits = features.infer(video);
+//   console.log(logits.dataSync());
+//   logits.print();
+// }
+
+// function modelReady() {
+//   console.log("Model is ready");
+// }
+
+// function draw() {
+//   image(video, 0, 0);
+// }
+
+// console.log(p5);
+
+import p5 from "p5";
+import ml5 from "ml5";
+
+let video;
+let features;
+
+const containerElement = document.getElementById("p5-container");
+
+const sketch = (p) => {
+  p.setup = function () {
+    // p.createCanvas(800, 400);
+
+    p.createCanvas(600, 400);
+
+    video = p.createCapture(p5.VIDEO);
+
+    video.size(600, 400);
+
+    video.hide();
+
+    features = ml5.featureExtractor("MobileNet", modelReady);
+  };
+
+  p.draw = function () {
+    p.image(video, 0, 0);
+  };
+
+  p5.mousePressed = function () {
+    const logits = features.infer(video);
+    console.log(logits.dataSync());
+    logits.print();
+  };
+
+  function modelReady() {
+    console.log("Model is ready");
+  }
+};
+
+new p5(sketch, containerElement);
