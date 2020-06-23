60c1df4418068a0f150b054cc56b8886a5affb2f
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-06-23 20:56:26 +0600

    Basic function is working properly - added useful comments

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ let features;
 let knn;
 let labelP;
 let ready = true;
+let endTrainingData = false;
+let trainingFinished = false;
+let logitsContainer = [];
 const containerElement = document.getElementById("p5-container");
+
 const sketch = (p) => {
   p.setup = function () {
     p.createCanvas(600, 400);
@@ -22,30 +26,50 @@ const sketch = (p) => {
   p.draw = function () {
     //drawing the element on the dom
     p.image(video, 0, 0);
-
-    //running the statement only one if there are any labels
-    if (knn.getNumLabels() > 0 && ready) {
-      console.log("====================================");
+    //The statement will run only once when all of its condition fulfilled
+    if (knn.getNumLabels() > 0 && ready && trainingFinished) {
       goClassify();
       ready = false;
+      console.log(p);
     }
   };
 
   //handling key press and training the model
-  p.keyPressed = function () {
-    //takes a instance of the video right at the moment when any key pressed
-    const logits = features.infer(video);
-    // TRAINING THE KNN MODEL
-    if (p.key == "l") {
-      //if the pressed key is 'l', adding that video instance(logits) and categorizing it as 'left'
-      knn.addExample(logits, "left");
-      console.log("left");
-    } else if (p.key == "r") {
-      knn.addExample(logits, "right");
-      console.log("right");
-    } else if (p.key == "u") {
-      knn.addExample(logits, "up");
-      console.log("up");
+  p._onkeydown = function () {
+    if (!endTrainingData) {
+      //takes a instance of the video right at the moment when any key pressed
+      const logits = features.infer(video);
+      console.log(p.keyCode);
+      // TRAINING THE KNN MODEL
+      if (p.keyCode == 37) {
+        console.log("left", logitsContainer.length);
+        logitsContainer.push({ logits, class: "left" });
+      } else if (p.keyCode == 39) {
+        console.log("right", logitsContainer.length);
+        logitsContainer.push({ logits, class: "right" });
+      } else if (p.keyCode == 38) {
+        console.log("up", logitsContainer.length);
+        logitsContainer.push({ logits, class: "up" });
+      } else if (p.keyCode == 40) {
+        console.log("down", logitsContainer.length);
+        logitsContainer.push({ logits, class: "down" });
+      }
+      //if the user presses enter, gathering all data and training the model
+      else if (p.keyCode == 13) {
+        //stopping keyPress-events so the user can't add any more data
+        endTrainingData = true;
+
+        labelP.html(`Training, ${logitsContainer.length}`);
+
+        logitsContainer.map((data) => {
+          //training the model based on data from logitsContainer
+          knn.addExample(data.logits, data.class);
+        });
+        labelP.html("Training Completed");
+
+        //setting trainingFinished to true, so we can start displaying the result
+        trainingFinished = true;
+      }
     }
   };
 
@@ -59,7 +83,7 @@ const sketch = (p) => {
         // displaying the result on the dom
         labelP.html(result.label);
         labelP.style("font-size", "64pt");
-        //doing the classification continuously, so after a good training, the video recorder will capture every moment and shows it result
+        //doing the classification continuously, so after a good training, the video recorder will capture every moment with a 50ms gape. and shows it result
         setTimeout(() => {
           goClassify();
         }, 50);
