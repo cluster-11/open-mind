43f45747e73c1226bbd3fd8531d94ae7df24fac9
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-06-26 22:16:17 +0600

    Removed p5js and went vanilla JavaScript - basic styling is complete - need to refactor code

--- src/index.js
+++ src/index.js
@@ -1,46 +1,43 @@
-import p5 from "p5";
 import ml5 from "ml5";
+import "./styles.css";
 
-const video1Container = document.querySelector(".video1-container");
-const video2Container = document.querySelector(".video2-container");
-const video3Container = document.querySelector(".video3-container");
-const showResult = document.querySelector(".show-result");
 const trainBtn1 = document.querySelector(".btn-1");
 const trainBtn2 = document.querySelector(".btn-2");
 const submitBtn = document.querySelector(".submit-btn");
+const imageCounter1 = document.querySelector(".image-counter1");
+const imageCounter2 = document.querySelector(".image-counter2");
+const resultText = document.querySelector(".result-text");
 
-let video1;
-let video2;
-let video3;
+let video1 = document.getElementById("video1");
+let video2 = document.getElementById("video2");
+let video3 = document.getElementById("video3");
 let ml5Features = ml5.featureExtractor("MobileNet", () => {});
 let knn = ml5.KNNClassifier();
 
-//adding video1 as a video on the dom
-const sketch = (p) => {
-  p.setup = function () {
-    p.createCanvas(600, 400);
-    video1 = p.createCapture(p5.VIDEO);
-    video1.size(600, 400);
-  };
-};
-
-//adding video2 as a video on the dom
-const sketch2 = (p) => {
-  p.setup = function () {
-    p.createCanvas(600, 400);
-    video2 = p.createCapture(p5.VIDEO);
-
-    video2.size(600, 400);
-  };
-};
-
-function addData(v, className) {
-  console.log(v);
-  const logits = ml5Features.infer(v);
-  knn.addExample(logits, className);
-  console.log("Added example for", className);
+//setting up the video capture component
+async function setUpVideo(v) {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+  });
+  v.srcObject = stream;
+  v.play();
 }
 
+setUpVideo(video1);
+setUpVideo(video2);
+video3.style.display = "none";
+resultText.style.display = "none";
+
+//adding data, training the model
+function addData(v, className) {
+  const logits = ml5Features.infer(v);
+  knn.addExample(logits, className);
+  const totalImage = knn.getCount();
+  imageCounter1.innerText = `${totalImage[0] ? totalImage[0] : 0}/ minimum 10`;
+  imageCounter2.innerText = `${totalImage[1] ? totalImage[1] : 0}/ minimum 10`;
+}
+
+//getting the result
 function getResult(v) {
   const logits = ml5Features.infer(v);
   //classifying the video instance (logits) and getting the result based on training data
@@ -49,7 +46,7 @@ function getResult(v) {
       console.error(error);
     } else {
       //showing result on the dom
-      showResult.innerText = result.label;
+      resultText.innerText = result.label;
       //running the classification function continuously.
       setTimeout(() => {
         getResult(v);
@@ -67,23 +64,18 @@ trainBtn2.addEventListener("click", () => {
 });
 
 submitBtn.addEventListener("click", () => {
-  video1.hide();
-  video2.hide();
-
-  //adding video3 as a video on the dom
-  const sketch3 = (p) => {
-    p.setup = function () {
-      p.createCanvas(600, 400);
-      video3 = p.createCapture(p5.VIDEO);
-      video3.size(600, 400);
-    };
-  };
-  new p5(sketch3, video3Container);
-
+  //removing some element and adding some element
+  video1.style.display = "none";
+  video2.style.display = "none";
+  trainBtn1.style.display = "none";
+  trainBtn2.style.display = "none";
+  submitBtn.style.display = "none";
+  imageCounter1.style.display = "none";
+  imageCounter2.style.display = "none";
+  video3.style.display = "inline";
+  resultText.style.display = "block";
+  setUpVideo(video3);
   getResult(video3);
 });
 
-new p5(sketch, video1Container);
-new p5(sketch2, video2Container);
-
-//TODO: save the model and later when users reloads the page, ask them if they want to use the pertained model
+//TODO: 1. save the model and later when users reloads the page, ask them if they want to use the pertained model
