09566653ba611c492c01ba66f3bd4046e6fab837
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-07-05 19:18:55 +0600

    Changed code structure and how it works - now the class name example doesn't depend on input value - we handle the input value on get result

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-//Add the second image data first, the bug will appear
+//Not expected result? One of the reason could be not enough example images
 
 import ml5 from "ml5";
 import "./styles.css";
 
 //dom-element
 const cs1TrainBtn = document.querySelector("#btn-1");
-const cs2TrainBtn = document.querySelector("#btn-2");
+const cs2CaptureVideo = document.querySelector("#btn-2");
 const submitBtn = document.querySelector("#submit-btn");
 const cs1ImageCounter = document.querySelector("#cs1-image-counter");
 const cs2ImageCounter = document.querySelector("#cs2-image-counter");
@@ -37,12 +37,11 @@ resultText.style.display = "none";
 
 //adding data, training the model
 function addData(fromVideo, className, customImgEvent = undefined) {
-  console.log("Getting image data");
   //if the user uploads image instead of capturing them from the video
   if (customImgEvent) {
     //if the user uploads multiple image
     [...customImgEvent.target.files].map((i) => {
-      const newImg = new Image(1, 1);
+      const newImg = new Image(460, 345);
       const imgSrc = window.URL.createObjectURL(i);
       //Preference: hiding the img element, we don't want to show it on this application
       newImg.style.display = "none";
@@ -58,29 +57,23 @@ function addData(fromVideo, className, customImgEvent = undefined) {
           totalImage[1] ? totalImage[1] : 0
         }/ minimum 10`;
       };
-      console.log("feeded image data");
-      console.log(knn.getCountByLabel(cs1Name.value));
-      console.log(knn.getCountByLabel(cs2Name.value));
     });
   }
   //default, capturing from the the video
   else {
-    console.log("Getting video data");
-
     const logits = ml5Features.infer(fromVideo);
     knn.addExample(logits, className);
-    const totalImage = knn.getCount();
+
+    const totalExample = knn.getCountByLabel();
+    console.log(totalExample);
+    //showing total totalExample on the dom
     cs1ImageCounter.innerText = `${
-      totalImage[0] ? totalImage[0] : 0
+      totalExample["class1"] ? totalExample["class1"] : 0
     }/ minimum 10`;
     cs2ImageCounter.innerText = `${
-      totalImage[1] ? totalImage[1] : 0
+      totalExample["class2"] ? totalExample["class2"] : 0
     }/ minimum 10`;
   }
-  console.log("feeded video data");
-  console.log(knn);
-  console.log(knn.getCountByLabel()[cs1Name.value]);
-  console.log(knn.getCountByLabel()[cs2Name.value]);
 }
 
 //getting the result
@@ -91,8 +84,13 @@ function getResult(v) {
     if (error) {
       console.error(error);
     } else {
-      //showing result on the dom
-      resultText.innerText = result.label;
+      console.log(result.label);
+      if (result.label == "class1") {
+        //showing result on the dom
+        resultText.innerText = cs1Name.value;
+      } else if (result.label == "class2") {
+        resultText.innerText = cs2Name.value;
+      }
       //running the classification function continuously, with a 40 milliseconds break
       setTimeout(() => {
         getResult(v);
@@ -101,49 +99,28 @@ function getResult(v) {
   });
 }
 
-//changing the class name
-function changeClassName(newName, index) {
-  if (knn.knnClassifier.mapStringToIndex) {
-    knn.knnClassifier.mapStringToIndex[index] = newName;
-  }
-}
-
-cs1TrainBtn.addEventListener("click", () => {
-  addData(video1, cs1Name.value);
-});
-
-cs2TrainBtn.addEventListener("click", () => {
-  addData(video2, cs2Name.value);
+cs1TrainBtn.addEventListener("mousedown", () => {
+  addData(video1, "class1");
 });
 
 cs1ImageUpload.addEventListener("change", (e) => {
-  addData(undefined, cs1Name.value, e);
+  addData(undefined, "class1", e);
+});
+
+cs2CaptureVideo.addEventListener("mousedown", () => {
+  addData(video2, "class2");
 });
 
 cs2ImageUpload.addEventListener("change", (e) => {
-  addData(undefined, cs2Name.value, e);
-});
-
-cs1Name.addEventListener("keyup", () => {
-  //first class starts ar 0
-  changeClassName(cs1Name.value, 0);
-});
-
-cs2Name.addEventListener("keyup", () => {
-  //second class starts ar 1
-  changeClassName(cs2Name.value, 1);
+  addData(undefined, "class2", e);
 });
 
 submitBtn.addEventListener("click", () => {
-  // Important, final check if the class names are as expected
-  changeClassName(cs1Name.value, 0);
-  changeClassName(cs2Name.value, 1);
-
   //removing some element and adding some element
   video1.style.display = "none";
   video2.style.display = "none";
   cs1TrainBtn.style.display = "none";
-  cs2TrainBtn.style.display = "none";
+  cs2CaptureVideo.style.display = "none";
   submitBtn.style.display = "none";
   cs1ImageCounter.style.display = "none";
   cs2ImageCounter.style.display = "none";
