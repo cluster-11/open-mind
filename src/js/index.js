5375627c50fbf17bf73a0c1f4244b6047ad80a2e
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-07-10 21:31:32 +0600

    Added comment

--- src/js/index.js
+++ src/js/index.js
@@ -24,6 +24,6 @@ export let knn = KNNClassifier;
 
 //CLOSED FOR DEVELOPMENT
 //ask before leaving/quitting the application
-window.onbeforeunload = function () {
-  return "Changes you made may not be saved";
-};
+// window.onbeforeunload = function () {
+//   return "Changes you made may not be saved";
+// };
