f49576c56b326e60253223eaa582f518e409e36b
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-07-20 01:18:12 +0600

    Moved from dist to docs - webpack now spits production data into docs folder

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
   mode: "production",
   output: {
     filename: "main.[contentHash].js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
   },
   optimization: {
     minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()],
