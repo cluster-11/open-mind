3fc58a0447cd0c13cfafc79a00e080d8669ec168
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-07-27 02:31:41 +0600

    Updated a little bit style and some todo comment

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,14 @@
 //Not expected result? One of the reason could be not enough example images
 //want to integrate this kind of feature? see the starter template or use this
 //facing bug/issues, create a github issue here
-//like this project? give it a star
-//application using unet, webpack, javascript....
 //view the application on github
 // about- this project is part of cluster-11 organization
 //view a similar application from google teachable machine
-//download the model
 //remove console.log
-//test on gpu slowdown, create new js for mobile
 //use teachable machine show result css
-//ask for images if none is provided [there are not enough sample images to train  this model alert]
-//add the main-header as most voted type on result-video
 //show good job when they add more images
 //show clear all example in a dropdown beside the button
-//show a reload button, in the result page
-//show `download dataset` and load `new dataset side` by side
+
 //[
 
 //how this application works, a short message, we don't save your images, all training happens on the browser,     Try to give as much example image as possible to get the best
@@ -24,6 +17,7 @@
 //provide example images, so the machine learning model gets enough
 //data to understand the difference between your two image classes and
 //prepare the result.
+//This is a open source project
 //]
 
 //try display none
@@ -31,6 +25,7 @@
 import "../styles.css";
 import "./handle-training";
 import "./handle-result";
+// Using custom model instead from the `ml5` cdn. It gives us much more flexibility
 import Mobilenet from "./knn-classifier&mobileNet/mobileNet";
 import KNNClassifier from "./knn-classifier&mobileNet/knn-classifier";
 import { noWrReload } from "./handle-result";
@@ -40,9 +35,9 @@ export let knn = new KNNClassifier();
 
 //CLOSED FOR DEVELOPMENT
 //ask before leaving/quitting the application
-window.onbeforeunload = function () {
-  //if user click on `train again` button, don't show them the warning
-  if (!noWrReload) {
-    return "Changes you made may not be saved";
-  }
-};
+// window.onbeforeunload = function () {
+//   //if user click on `train again` button, don't show them the warning
+//   if (!noWrReload) {
+//     return "Changes you made may not be saved";
+//   }
+// };
